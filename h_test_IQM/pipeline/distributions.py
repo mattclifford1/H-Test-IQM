a3707d74756a289f37f1243354ddd1af8386a103
Matt Clifford <matt.clifford@bristol.ac.uk>
2024-08-09 10:39:41 +0100

    both dists

--- h_test_IQM/pipeline/distributions.py
+++ h_test_IQM/pipeline/distributions.py
@@ -1,4 +1,38 @@
 import numpy as np
+import multiprocessing
+from tqdm import tqdm
+
+
+def test_single(args):
+    results = get_IQM_sensitivity(
+        img=args[0], IQM=args[1], distortion=args[2], num_samples=10)
+    return results
+
+def get_all_scores(im_iter, IQM, distortion, num_samples=10, mp=False):
+
+    # cannot use multiprocessing in with many IQMs currently...
+    if mp == True:
+        n_cpus = multiprocessing.cpu_count()
+        args = [(img, IQM, distortion) for img in im_iter]
+        with multiprocessing.Pool(processes=n_cpus) as pool:
+            scores = list(tqdm(pool.imap_unordered(test_single, args),
+                               total=len(im_iter), leave=False))
+
+    else:
+        scores = []
+        for img in tqdm(im_iter):
+            results = get_IQM_sensitivity(
+                img, IQM=IQM, distortion=distortion, num_samples=10)
+            scores.append(results)
+
+    # convert all into one
+    all_scores = []
+    for results in scores:
+        for sample in results:
+            all_scores.append(sample)
+    return all_scores
+
+
 
 
 def get_IQM_sensitivity(img, IQM, distortion, num_samples=1000):
