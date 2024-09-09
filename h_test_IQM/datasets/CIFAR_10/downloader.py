ead0f08192fedb0768a0626fe057db645d0c5477
Matt Clifford <matt.clifford@bristol.ac.uk>
2024-09-09 16:04:34 +0100

    cp cifar and imageanet loaders

---
+++ h_test_IQM/datasets/CIFAR_10/downloader.py
@@ -0,0 +1,76 @@
+import os
+import shutil
+import pickle
+import pandas as pd
+import numpy as np
+from tqdm import tqdm
+from PIL import Image
+import torchvision
+from percept_loss.datasets.CIFAR_10.VARS import CIFAR_10_RAW_DATA_DIR, CIFAR_10_META_CSV, CIFAR_10_IMAGE_DIR
+
+
+def download_CIFAR_10(redo_download=False):
+    if redo_download == True:
+        shutil.rmtree(CIFAR_10_RAW_DATA_DIR)
+    if not os.path.exists(CIFAR_10_RAW_DATA_DIR):
+        # use torch vision to download CIFAR-10 and unzip
+        _ = torchvision.datasets.CIFAR10(root=CIFAR_10_RAW_DATA_DIR, train=True,
+                                            download=True, transform=None)
+        _ = torchvision.datasets.CIFAR10(root=CIFAR_10_RAW_DATA_DIR, train=False,
+                                            download=True, transform=None)
+    # delete tar file
+    comp = os.path.join(CIFAR_10_RAW_DATA_DIR, 'cifar-10-python.tar.gz')
+    if os.path.exists(comp):
+        os.remove(comp)
+    
+    # save to png
+    if not os.path.exists(CIFAR_10_IMAGE_DIR):
+        save_batches_as_png(CIFAR_10_IMAGE_DIR)
+
+    bin_data = os.path.join(CIFAR_10_RAW_DATA_DIR, 'cifar-10-batches-py')
+    if os.path.exists(bin_data):
+        shutil.rmtree(bin_data)
+
+
+def unpickle(file):
+    with open(file, 'rb') as fo:
+        dict = pickle.load(fo, encoding='bytes')
+    return dict
+
+
+def save_batches_as_png(ims_dir):
+    batches = ['data_batch_1',
+               'data_batch_2',
+               'data_batch_3',
+               'data_batch_4',
+               'data_batch_5',
+               'test_batch']
+    file_names = []
+    labels = []
+    one_hot_labels = []
+    label_names = unpickle(os.path.join(CIFAR_10_RAW_DATA_DIR, 'cifar-10-batches-py', 'batches.meta'))
+    label_names = [x.decode('utf-8') for x in label_names[b'label_names']]                    
+    os.makedirs(ims_dir, exist_ok=True)
+    for batch in tqdm(batches):
+        batch_dict = unpickle(os.path.join(CIFAR_10_RAW_DATA_DIR, 'cifar-10-batches-py', batch))
+        for i, file in tqdm(enumerate(batch_dict[b'filenames']), leave=False):
+            filename = file.decode("utf-8")
+            im = batch_dict[b'data'][i, :].reshape(3, 32, 32)
+            im = np.transpose(im, (1, 2, 0))
+            im = Image.fromarray(im)
+            im.save(os.path.join(ims_dir, filename))
+
+            file_names.append(filename)
+            label = int(batch_dict[b'labels'][i])
+            one_hot_labels.append(label) 
+            labels.append(label_names[int(label)])
+
+    meta_dict = {'filename': file_names, 
+                 'label': labels,
+                 'numerical_label': one_hot_labels}
+    df = pd.DataFrame.from_dict(meta_dict)
+    df.to_csv(CIFAR_10_META_CSV, index=False, header=list(meta_dict.keys()))
+
+
+if __name__ == '__main__':
+    download_CIFAR_10(redo_download=True)
---
+++ h_test_IQM/datasets/CIFAR_10/loader.py
@@ -0,0 +1,115 @@
+import os
+from tqdm import tqdm
+import pandas as pd
+import torch
+from torchvision.io import read_image
+from percept_loss.datasets.CIFAR_10.VARS import CIFAR_10_META_CSV, CIFAR_10_IMAGE_DIR
+from percept_loss.datasets.CIFAR_10.downloader import download_CIFAR_10
+
+
+class CIFAR_10_LOADER:
+    '''
+    Generic data loader for CIFAR-10
+    Args:
+        indicies_to_use: list of in the dataset to use if you require to use a split of the dataset
+        image_dict: dictionary of pre loaded images filename as keys and torch tensor as values 
+    '''
+    def __init__(self, indicies_to_use='all', 
+                 image_dict={}, 
+                 cache_data=True,
+                 normalise=(0, 1),
+                 dtype=torch.float32,
+                 device='cpu'):
+        self.indicies_to_use = indicies_to_use
+        self.image_dict = image_dict
+        self.cache_data = cache_data
+        self.normalise = normalise
+        self.dtype = dtype
+        self.device = device
+
+        # make sure dataset is downloaded
+        download_CIFAR_10()    
+
+        self.label_cache = {}
+        self.meta_data = pd.read_csv(CIFAR_10_META_CSV)
+        self.filenames = self.meta_data['filename'].to_list()
+        self.labels = self.meta_data['label'].to_list()
+        self.numerical_label = self.meta_data['numerical_label'].to_list()
+        self.num_classes = 10
+        self.reduce_data()
+
+    def reduce_data(self):
+        if self.indicies_to_use == 'all':
+            return
+        else:
+            self.filenames = [self.filenames[i] for i in self.indicies_to_use]
+            self.labels = [self.labels[i] for i in self.indicies_to_use]
+            self.numerical_label = [self.numerical_label[i] for i in self.indicies_to_use]
+
+    def get_images_dict(self):
+        '''get dict of all pre loaded and processed images - useful to pass to other dataset
+        so that you only have to read from file and process once'''
+        images = {}
+        for ind in tqdm(range(len(self.labels)), desc='Loading CIFAR-10', leave=False):
+            filename = self.filenames[ind]
+            # get image
+            image = self._get_image(filename)
+            images[filename] = image
+        return images
+
+    def _get_image(self, filename):
+        # use cache data if loaded already
+        if filename in self.image_dict:
+            image = self.image_dict[filename]
+        else:
+            # load if not cached
+            path = os.path.join(CIFAR_10_IMAGE_DIR, filename)
+            image = read_image(path)
+            image = image.to(self.dtype)
+            image = image/255
+            image = image*(self.normalise[1]-self.normalise[0])
+            image = image + self.normalise[0]
+            image = image.to(self.device)
+            if self.cache_data == True:
+                self.image_dict[filename] = image
+        return image
+    
+    def _get_labels(self, ind):
+        if ind in self.label_cache:
+            one_hot = self.label_cache[ind]['one_hot']
+            label = self.label_cache[ind]['label']
+        else:
+            one_hot = torch.zeros(self.num_classes, dtype=self.dtype)
+            one_hot[self.numerical_label[ind]] = 1
+            # one_hot = one_hot.to(self.device)
+            label = torch.tensor(self.numerical_label[ind])
+            # label = label.to(self.device)
+            if self.cache_data == True:
+                self.label_cache[ind] = {'label':{}, 'one_hot':{}}
+                self.label_cache[ind]['one_hot'] = one_hot
+                self.label_cache[ind]['label'] = label
+        return one_hot, label
+
+    def __len__(self):
+        return len(self.labels)
+
+    def __getitem__(self, ind):
+        filename = self.filenames[ind]
+        # get image
+        image = self._get_image(filename)
+        # get labels
+        one_hot, label = self._get_labels(ind)
+        return image, one_hot, label
+    
+
+if __name__ == '__main__':
+    from tqdm import tqdm
+    a = CIFAR_10_LOADER()
+    ims = a.get_images_dict()
+
+    b = CIFAR_10_LOADER(image_dict=ims)
+
+    # benchmark
+    for e in tqdm(range(10)):
+        for i in tqdm(b, leave=False):
+            pass
